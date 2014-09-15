abdcbaaa41078c76eedb510f95fc5c6b78990e63
Amber Douglas <douglasamber3@gmail.com>
2014-09-14 21:31:17 -0700

    2nd push
    testing media coding

---
+++ style.js
@@ -0,0 +1,11 @@
+    $.fx.step.textShadowBlur = function(fx) {
+        $(fx.elem).css({textShadow: '0 0 ' + Math.floor(fx.now) + 'px black'});
+    };
+    
+
+setInterval(function() {
+    
+    $("#seconds").animate({textShadowBlur:20}, {duration: 300, complete: function() { 
+        $("#seconds").animate({textShadowBlur:0}, {duration: 300}); 
+    }});
+}, 1000);
